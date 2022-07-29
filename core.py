d63a236a653b896e4eef89ca4a6816ee9bf013db
Alex Kollar <cythesout@gmail.com>
2022-07-29 03:32:04 -0400

    Initial Code Upload

---
+++ core.py
@@ -0,0 +1,48 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from os import system
+from modules import menus
+import os 
+import sys
+
+banner = """
+    _   __            _ 
+   / | / /___ __   __(_)
+  /  |/ / __ `/ | / / / 
+ / /|  / /_/ /| |/ / /  
+/_/ |_/\__,_/ |___/_/   
+                                                                                                                                                            
+"""
+
+
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+print(banner)
+print("\nType 'help' to show a list of commands:\n")
+
+for steps in range(sys.maxsize**10):
+    text = input(">> ")
+    if text == "help":
+        print("Navi: Maybe this will help you\n")
+        print(menus.bothelp)
+    if text == "cls":
+        system('clear')
+    if text == "reset":
+        print(f"Navi: I am sorry I let you down, lets try again :D")
+        os.execl(sys.executable, sys.executable, *sys.argv)
+    if text == "exit":
+        print("Navi: Thank you for talking me! I look forward to seeing you again!")
+        exit()
+    else:
+        input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+        bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if steps > 0 else input_ids
+        # generate a bot response
+        chat_history_ids = model.generate(
+            bot_input_ids,
+            max_length=1000,
+            pad_token_id=tokenizer.eos_token_id,
+        )
+        #print the output
+        output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        print(f"Navi: {output}")
---
+++ modules/menus.py
@@ -0,0 +1,12 @@
+bothelp = """
+   +--------------------------------------------+ 
+   | Basic Usage:                               |
+   | To use the AI simply start chatting        |
+   | it will respond as best it can.            |
+   + -------------------------------------------+
+   | Commands:                                  |
+   | reset - Resets the chat session back to 0  |
+   | cls   - Clears the screen                  |
+   | exit  - exits the current session          |
+   +--------------------------------------------+ 
+"""
