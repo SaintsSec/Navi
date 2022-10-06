2b2bbc417a7f757507dd0281e4aa7082861fe1e6
Alex Kollar <cythesout@gmail.com>
2022-10-05 20:56:06 -0400

    Removed personality and menu functions for now.

--- core.py
+++ core.py
@@ -13,20 +13,20 @@ import torch, os, sys
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 print(menus.banner)
-personality.lifelike()
+# personality.lifelike()
 for steps in range(sys.maxsize**10):
     text = input(">> ")
-    if text == "help" or "-h":
-        print("Navi: Maybe this will help you\n")
-        print(menus.bothelp)
-    if text == "cls" or "clear":
-        system('clear')
-    if text == "reset" or "reboot":
-        print(f"Navi: Rebooting... lets try again :D")
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    if text == "goodbye" or "Goodbye" or "See you later":
-        print("Navi: Thank you for talking me! I look forward to seeing you again!")
-        exit()
+    # if text == "help" or "-h":
+    #     print("Navi: Maybe this will help you\n")
+    #     print(menus.bothelp)
+    # if text == "cls" or "clear":
+    #     system('clear')
+    # if text == "reset" or "reboot":
+    #     print(f"Navi: Rebooting... lets try again :D")
+    #     os.execl(sys.executable, sys.executable, *sys.argv)
+    # if text == "goodbye" or "Goodbye" or "See you later":
+    #     print("Navi: Thank you for talking me! I look forward to seeing you again!")
+    #     exit()
     else:
         input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
         bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if steps > 0 else input_ids
