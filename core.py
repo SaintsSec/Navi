ee952d36a6954b07eeb28d3a73b1ec35c4ebe7f7
Alex Kollar <cythesout@gmail.com>
2022-10-05 21:05:57 -0400

    Had to remove personality and menu for now.

--- core.py
+++ core.py
@@ -16,26 +16,14 @@ print(menus.banner)
 # personality.lifelike()
 for steps in range(sys.maxsize**10):
     text = input(">> ")
-    # if text == "help" or "-h":
-    #     print("Navi: Maybe this will help you\n")
-    #     print(menus.bothelp)
-    # if text == "cls" or "clear":
-    #     system('clear')
-    # if text == "reset" or "reboot":
-    #     print(f"Navi: Rebooting... lets try again :D")
-    #     os.execl(sys.executable, sys.executable, *sys.argv)
-    # if text == "goodbye" or "Goodbye" or "See you later":
-    #     print("Navi: Thank you for talking me! I look forward to seeing you again!")
-    #     exit()
-    else:
-        input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
-        bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if steps > 0 else input_ids
-        # generate a bot response
-        chat_history_ids = model.generate(
-            bot_input_ids,
-            max_length=1000,
-            pad_token_id=tokenizer.eos_token_id,
-        )
-        #print the output
-        output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-        print(f"Navi: {output}")
+    input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors="pt")
+    bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if steps > 0 else input_ids
+    # generate a bot response
+    chat_history_ids = model.generate(
+        bot_input_ids,
+        max_length=1000,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    #print the output
+    output = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    print(f"Navi: {output}")
