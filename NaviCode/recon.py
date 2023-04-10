73cef60d926379e46cd231ce0b60179fce57130c
Alex Kollar <ssgalexkollar@proton.me>
2023-04-10 19:44:00 -0400

    Made recon art reflect where it was. Added ping scan and break lines to output

--- NaviCode/recon.py
+++ NaviCode/recon.py
@@ -1,4 +1,5 @@
 #! /bin/python3
+
 import os, subprocess, time
 from mods import mods
 
@@ -6,19 +7,25 @@ breakline = mods.breakline
 art = mods.art
 
 print(art)
+
 def main():
-    #Nmap Setup
-    scanIP = input("Navi> Give me an IP to scan: ")
+    #Recon Setup
+    targetIP = input("Navi> Give me an IP to scan: ")
     scanOptions = input("Navi> What type of scan (ex: -sV): ")
     reportName = input("Navi> What would you like to name the report(ex: Report1): ")
     print(breakline + "\n\n")
 
     #Scan execution
     print("The Process:")
-    print(f"[\u2713] - Starting Scan now: nmap {scanOptions} {scanIP}")
-    os.system(f"nmap {scanOptions} {scanIP} > ~/{reportName}.txt")
-    print("[\u2713] - Scan complete!!")
-    print(f"[\u2713] - Showing generated report now! \nScan can be viwed anytime at: ~/{reportName}.txt")
+    print(f"Navi> [!] - Pinging host to see if its alive...")
+    os.system(f'ping -c 4 {targetIP} >> ~/{reportName}.txt')
+    os.system(f'echo {breakline} >> ~/{reportName}.txt')
+    print(f"Navi> [\u2713] - Ping scan complete!")
+    time.sleep(2)
+    print(f"Navi> [!] - Starting Scan now: nmap {scanOptions} {targetIP}")
+    os.system(f"nmap {scanOptions} {targetIP} >> ~/{reportName}.txt")
+    print("Navi> [\u2713] - nmap scan complete!!")
+    print(f"Navi> [\u2713] - Showing generated report now! \nScan can be viwed anytime at: ~/{reportName}.txt")
 
     #Show nmap report
     print("\n\n" + breakline)
