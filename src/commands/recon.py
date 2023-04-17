b8285eeb4026fbf1da2041a172c77db8cd6106f8
Alex Kollar <ssgalexkollar@proton.me>
2023-04-17 02:12:50 -0400

    Viewing saved recon report is now optional

--- src/commands/recon.py
+++ src/commands/recon.py
@@ -13,16 +13,17 @@ breakline = mods.breakline
 def run():
     """Do recon."""
     # Recon Setup
-    print("Nmap Setup:")
+    print("\nNmap Setup:")
     targetIP = input("Navi> Give me an IP or URL to scan: ")
     scanOptions = input("Navi> What type of scan (ex: -sV): ")
-    print("WhoIs Setup:")
+    print("\nWhoIs Setup:")
     targetUrl = input("Navi> What URL should I look into: ")
+    print("\nReport Setup:")
     reportName = input("Navi> Save report as(ex: Report1): ")
-    print(breakline + "\n")
+    print("\n" + breakline)
     
     # Scan execution
-    print("The Process:")
+    print("\nThe Process:")
     print("Navi> [!] - Pinging host to see if its alive...")
     os.system(f'ping -c 4 {targetIP} >> ~/{reportName}.txt')
     os.system(f'echo {breakline} >> ~/{reportName}.txt')
@@ -40,12 +41,19 @@ def run():
     os.system(f"echo {breakline} >> ~/{reportName}.txt")
     os.system(f"whois {targetUrl} >> ~/{reportName}.txt")
     print(f"Navi> [\u2713] - Whois on {targetUrl} complete")
-    time.sleep(2)
 
-    #Show Report
-    print("Navi> [\u2713] - Showing generated report now!")
-    print(f"\n[!!]View this report anytime at: ~/{reportName}.txt[!!]")
-    print("\n" + breakline)
-    time.sleep(2)
-    os.system(f"cat ~/{reportName}.txt")
-    print(breakline)
+    #Report loop
+    reportChoice = input("\nNavi> [\u2713] - RECON COMPLETE \nDo you want to see the report now? (yes / no): ").lower()
+    if reportChoice == "yes":
+        print("\nNavi> [\u2713] - Showing generated report now!")
+        print(f"\n[!!]View this report anytime at: ~/{reportName}.txt[!!]")
+        print("\n" + breakline)
+        time.sleep(2)
+        os.system(f"cat ~/{reportName}.txt")
+        print(breakline)
+    if reportChoice == "no":
+        print(f"\nNavi> [\u2713] - That is fair. \nReport contents saved to: ~/{reportName}.txt\n\n{breakline}")
+        return
+    else:
+        print(f"\nNavi> [!!] - The report can be viewed at: ~/{reportName}.txt\n\n{breakline}")
+        return
