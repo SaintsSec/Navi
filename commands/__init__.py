f07cd82b5ccf12e6f7c372b9c6f9e49df710a13b
wyrd_ <80168699+wyrdCCS@users.noreply.github.com>
2023-11-16 23:12:31 -0600

    Update __init__.py
    Added logging to init to alert when modules import.

--- commands/__init__.py
+++ commands/__init__.py
@@ -2,6 +2,7 @@
 from os.path import dirname, basename, isfile, join
 import glob
 import importlib
+import logging
 
 
 def load_module(name):
@@ -10,6 +11,14 @@ def load_module(name):
         module = importlib.import_module(f".{name}", '.commands')
     except ModuleNotFoundError:
         print(f"Module '{name}' not found")
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(levelname)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            filename="logs/error.log",
+        )
+
+        logging.error(f"Module '{name}' not found")
         # exit(1)
     else:
         return module
