f4afb6c6f26790fb0cde9c657f6dcce0ded4040f
Alex Kollar <ssgalexkollar@proton.me>
2024-02-26 13:11:20 -0500

    Install script patched requirements, arch install

--- install/install.sh
+++ install/install.sh
@@ -36,12 +36,8 @@ install_reqs() {
             sudo pip install -U pyopenssl cryptography
             ;;
         "Arch") 
-            sudo pacman -Sy python3 python3-pip whois nmap
-            sudo pacman -Sy clamav 
-            python3 -m pip install --upgrade pip 
-            sudo pip install requests fpdf pyfiglet click tabulate openai
-            pip install -U pyopenssl cryptography
-            sudo pip install -U pyopenssl cryptography
+            sudo pacman -Sy python3 python3-pip whois nmap sudo pacman -Sy python python-pip python-requests python-fpdf python-pyfiglet python-click python-tabulate python-openai clamav 
+            python3 -m pip install --upgrade pip
             ;;
         "fedora") 
             sudo dnf install -y python3 python3-pip whois nmap 
