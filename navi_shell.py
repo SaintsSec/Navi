8b42d6b6b133f29d318e92026dee5088e5408b98
VAliquo <vincega13@gmail.com>
2024-06-04 13:29:28 -0400

    [REMOVE] Unused libraries

--- navi_shell.py
+++ navi_shell.py
@@ -53,125 +53,6 @@ def tr(text):
                 break
 
 
-def parse_arguments():
-    """Parse command-line arguments and return the parsed args object."""
-
-    # Custom formatter that retains any whitespace in the description or epilog.
-    parser = argparse.ArgumentParser(
-        description="Navi shell arguments.", formatter_class=argparse.RawTextHelpFormatter)
-
-    # Help and usage strings
-    q_help = (
-        "Quickly query the AI and print the response.\n"
-        "Use: navi -q "
-    )
-
-    nh_help = (
-        "List currently installed Navi custom scripts.\n"
-        "Use: navi -nh"
-    )
-
-    r_help = (
-        "Run a Navi custom script.\n"
-        "Use: navi -r /<command>"
-    )
-
-    parser.add_argument('-q', type=str, nargs='+', default=None,
-                        help=q_help)
-    parser.add_argument('-nh', action='store_true', help=nh_help)
-    parser.add_argument('-r', type=str, help=r_help)
-
-    # Add other arguments as necessary
-    return parser.parse_args()
-
-
-def handle_arguments(args):
-    """Handle the actions based on the provided arguments."""
-
-    if args.q:
-        responses = query_navi(args.q)
-        for response in responses:
-            tr(response)
-        return True
-
-    if args.r:
-        # Handle the -r argument logic here
-        if args.r in commands.modules.keys():
-            print(
-                f"{mods.breakline}\n{ai_name_rep} [\u2713] - Running command: '{args.r}'\n{mods.breakline}")
-            commands.modules[args.r].run()
-        return True
-
-    if args.nh:
-        directory_path = '/opt/Navi/commands'
-
-        # Filter for Python files and exclude __init__.py
-        files = [f for f in os.listdir(directory_path) if f.endswith(
-            '.py') and f != '__init__.py']
-
-        commands_and_uses = []
-
-        for file in files:
-            command = file.replace('.py', '')
-
-            # Load the module
-            spec = importlib.util.spec_from_file_location(
-                command, os.path.join(directory_path, file))
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-
-            # Grab the 'use' variable from the module
-            # Default to 'Description not found' if 'use' is not in the script
-            use_desc = getattr(module, 'use', 'Description not found')
-            commands_and_uses.append((command, use_desc))
-
-        # For alignment purposes
-        max_command_length = max([len(cmd) for cmd, _ in commands_and_uses])
-        max_use_length = max([len(use) for _, use in commands_and_uses])
-
-        print("\nCurrently installed Navi custom scripts:\n")
-        print(f"{'Command':<{max_command_length}} \t Description")
-        print('-' * max_command_length + '\t' + '-' *
-              max_use_length)  # Drawing separators
-
-        for command, use_desc in commands_and_uses:
-            print(f"{command:<{max_command_length}} \t {use_desc}")
-
-        print("\nuse navi -r /<command> to run a script")
-        return True
-
-    return False  # Indicate that no specific argument actions were handled
-
-
-# Navi Script Engine 
-def chip_engine_help():
-    print(f"""{helpArt}
-Navi is a simple to use customizable interface for CLI
-AI models. Built with cybersecurity professionals in mind. 
-
-[!!] - All commands are preceeded by a '/'
-Here is a list of current useable commands 
-
-Command:              Use:
-""")
-    for _, module in commands.modules.items():
-        print(module.command.ljust(21), module.use)
-    print(breakline)
-
-
-def chip_engine():
-    chip_engine_help()
-    message = input(f"{ai_name_rep} What do you want to run:  ")
-    if message[0] == "/":
-        if message in commands.modules.keys():
-            print(
-                f"{mods.breakline}\n{ai_name_rep} [\u2713] - Running command: '{message}'\n{mods.breakline}")
-            commands.modules[message].run()
-        if message == "":
-            print(
-                f"{ai_name_rep} [!!] - I did not catch that. Please try again.")
-
-
 def get_latest_release(repo_owner, repo_name):
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
     response = requests.get(api_url)
@@ -189,8 +70,8 @@ def get_latest_release(repo_owner, repo_name):
         }
     else:
         return None
-
-
+    
+    
 def is_new_release(current_version, latest_version):
     return current_version < latest_version
 
